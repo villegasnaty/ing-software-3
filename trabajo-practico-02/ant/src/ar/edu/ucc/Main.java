542b4a086dd7d44e80f458272117760d8ab02c31
Natalia Villegas <NVillegas@proveedor.personal.com.ar>
2018-09-17 18:56:27 -0300

    TP2, tp4

---
+++ trabajo-practico-02/ant/src/ar/edu/ucc/Main.java
@@ -0,0 +1,13 @@
+package ar.edu.ucc;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
